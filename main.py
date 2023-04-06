9d0140b0567384d653198c1e74310d034c18927c
agwaBom <khyunjin1993@gmail.com>
2023-04-06 16:36:41 +0900

    path_to_file_list implemented

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from typing import List
 
 def path_to_file_list(path: str) -> List[str]:
     """Reads a file and returns a list of lines in the file"""
-    # Will this be working?
+    # implemented as a one-liner, open in read mode
     lines = open(path, 'r').read().split('\n')
     return lines
 
@@ -14,7 +14,7 @@ def train_file_list_to_json(english_file_list: List[str], german_file_list: List
     def process_file(file):
         if '\\' in file:
             file = file.replace('\\', '\\\\')
-        if '/' or '"' in file:
+        if '/' in file or '"' in file:
             file = file.replace('/', '\\/')
             file = file.replace('"', '\\"')
         return file
